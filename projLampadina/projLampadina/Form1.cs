a7350ac6988119a503010a2d9f451c55f4fda4e7
ilFuretto <coolmike252@gmail.com>
2024-09-27 09:02:41 +0200

    Add files via upload

---
+++ projLampadina/projLampadina/Form1.cs
@@ -0,0 +1,38 @@
+namespace projLampadina
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = false;
+            pictureBox1.Visible = true;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
